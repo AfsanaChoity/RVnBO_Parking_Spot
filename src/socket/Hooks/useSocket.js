8c70fc9f06a862925ccae66624bb1e182da1db6f
Afsana Mimi Choity <afsanamimi194@gmail.com>
2025-09-11 16:34:27 +0600

    changed the backend URL  in socket server

--- src/socket/Hooks/useSocket.js
+++ src/socket/Hooks/useSocket.js
@@ -1,47 +1,11 @@
-// import { useEffect, useRef } from "react";
-// import { io } from "socket.io-client";
-
-// export const useSocket = (token) => {
-//   const socketRef = useRef(null);
-//   const backendUrl = "http://10.10.20.73:5000";
-
-
-//   useEffect(() => {
-//     if (!token) {
-//       console.warn("No token provided, socket will not be initialized.");
-//       return;
-//     }
-
-//     // create and store socket instance
-//     const skt = io(backendUrl, { auth: { token } });
-//     socketRef.current = skt;
-
-//     const onConnect = () => console.log("Socket connected:", skt.id);
-//     const onDisconnect = () => console.log("Socket disconnected");
-
-//     skt.on("connect", onConnect);
-//     skt.on("disconnect", onDisconnect);
-
-//     return () => {
-//       // clean up listeners and close connection
-//       skt.off("connect", onConnect);
-//       skt.off("disconnect", onDisconnect);
-//       skt.disconnect();
-//       socketRef.current = null;
-//     };
-//   }, [token]);
-
-//   // return the socket instance (null until connected)
-//   return socketRef.current;
-// };
-
 
 
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
 export const useSocket = (token) => {
-  const backendUrl = "http://10.10.20.73:5000";
+  // const backendUrl = "http://10.10.20.73:5000";
+  const backendUrl = "https://garyh-backend.onrender.com";
   const socketRef = useRef(null);
   const [socket, setSocket] = useState(null);
 
